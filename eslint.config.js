4ec0847d9087e54b727387261aa815a740d43f72
eleandro22 <eleandro.hoxha@gmail.com>
2025-01-12 13:56:04 +0100

    elsint edit

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,22 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": "warn",
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
 ];
